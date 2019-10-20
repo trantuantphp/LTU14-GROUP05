8264a8b7e041d99919ba9aea497b24b391ab4ed9
tuan tran <trantuantphp@gmail.com>
2019-10-20 13:27:43 +0700

    init backend

--- code/frontend/src/stores/NewsStore.js
+++
@@ -1,9 +0,0 @@
-import { observable } from 'mobx';
-
-class newsStore {
-    @observable hello = 'Hello World';
-    @observable selectedLang = 'vi';
-    @observable i18n = [];
-}
-
-export default newsStore;
